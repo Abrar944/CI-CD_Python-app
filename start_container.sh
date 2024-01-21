16b69547cd567206eea246b742b908ff93c40ce8
Mohammed Abrar Ahmed <70652985+Abrar944@users.noreply.github.com>
2024-01-21 18:20:50 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,11 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull ahmedbhai/simple-python-flask-app
+echo "Pulling Docker image..."
+docker pull ahmedbhai/simple-python-flask-app:tagname
+echo "Docker image pulled successfully."
 
 # Run the Docker image as a container
+echo "Running Docker container..."
 docker run -d -p 5000:5000 ahmedbhai/simple-python-flask-app
+echo "Docker container is running."
